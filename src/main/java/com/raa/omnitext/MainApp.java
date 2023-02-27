222378ea0cc6945e0a44aeec7368eb3266e4a92d
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-02-27 18:30:07 +0530

    initial setup #2

--- src/main/java/com/raa/omnitext/MainApp.java
+++ src/main/java/com/raa/omnitext/MainApp.java
@@ -1,17 +1,16 @@
 package com.raa.omnitext;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(new StackPane(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
